89095bf5d39dad5d3dba49de659b8542d7affba0
Miss Moss <samanthaberne@gmail.com>
2021-04-08 09:29:33 -0400

    will this please work

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 global.__basedir = __dirname;
+require('nunjucks');
 console.log('If you can see this log, that means this piece of nonsense is actually working. Woo-hoo!');
---
+++ test.js
@@ -0,0 +1,1 @@
+console.log('If you can see this, it means test.js worked. Now, go eat a foobar.');
